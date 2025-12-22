e828d9110957934254702b698d22b1ad607e9a7c
Lwin Min Thein <lwinthein679@gmail.com>
2025-12-22 15:21:24 +0630

    fix png type

--- src/App.tsx
+++ src/App.tsx
@@ -6,86 +6,78 @@ import { Draggable } from "gsap/all";
 import { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 
-
 function App() {
-const GRID_SIZE = 96; 
+  const GRID_SIZE = 96;
 
-
-  const containerRef=useRef<HTMLElement| null>(null);
-  const targerRef=useRef<HTMLDivElement | null>(null);
-  const cloneRef=useRef<HTMLDivElement | null>(null)
+  const containerRef = useRef<HTMLElement | null>(null);
+  const targerRef = useRef<HTMLDivElement | null>(null);
+  const cloneRef = useRef<HTMLDivElement | null>(null);
   gsap.registerPlugin(Draggable);
 
   const snap = (value: number) => Math.round(value / GRID_SIZE) * GRID_SIZE;
-const tl =gsap.timeline();
-useGSAP(() => {
-  if (!targerRef.current || !containerRef.current) return;
+  const tl = gsap.timeline();
+  useGSAP(
+    () => {
+      if (!targerRef.current || !containerRef.current) return;
 
-  Draggable.create(targerRef.current, {
-    type: "x,y",
-    inertia: false,
-    bounds: containerRef.current,
+      Draggable.create(targerRef.current, {
+        type: "x,y",
+        inertia: false,
+        bounds: containerRef.current,
+        onPress() {
+          const target = this.target as HTMLDivElement;
 
-    // liveSnap: {
-    //   x: (value) => Math.round(value / GRID_SIZE) * GRID_SIZE,
-    //   y: (value) => Math.round(value / GRID_SIZE) * GRID_SIZE,
-    // },
+          const clone = target.cloneNode(true) as HTMLDivElement;
+          clone.style.position = "absolute";
+          clone.style.pointerEvents = "none";
+          clone.style.opacity = "0.4";
 
-    onPress() {
-      
+          clone.style.left = target.offsetLeft + "px";
+          clone.style.top = target.offsetTop + "px";
 
-      
-      const target = this.target as HTMLDivElement;
+          containerRef.current!.appendChild(clone);
+          cloneRef.current = clone;
 
-      const clone = target.cloneNode(true) as HTMLDivElement;
-      clone.style.position = "absolute";
-      clone.style.pointerEvents = "none";
-      clone.style.opacity = "0.4";
+          target.style.opacity = "0.6";
+        },
 
-      clone.style.left = target.offsetLeft + "px";
-      clone.style.top = target.offsetTop + "px";
+        onRelease() {
+          const target = this.target as HTMLDivElement;
 
-      containerRef.current!.appendChild(clone);
-      cloneRef.current = clone;
+          gsap.to(target, {
+            x: snap(this.x),
+            y: snap(this.y),
+            duration: 0.1,
+            ease: "none",
+          });
 
-      target.style.opacity = "0.6";
-    },
+          if (cloneRef.current) {
+            cloneRef.current.remove();
+            cloneRef.current = null;
+          }
 
-    onRelease() {
-
-
-      const target = this.target as HTMLDivElement;
-
-       gsap.to(target, {
-        x: snap(this.x),
-        y: snap(this.y),
-        duration: 0.1,
-        ease: "none",
+          target.style.opacity = "1";
+        },
       });
-
-
-      if (cloneRef.current) {
-        cloneRef.current.remove();
-        cloneRef.current = null;
-      }
-
-      target.style.opacity = "1";
     },
-  });
-}, { scope: containerRef });
-
-
-
-
-
+    { scope: containerRef }
+  );
 
   return (
-    <main className="relative min-h-screen w-screen flex flex-col" ref={containerRef}>
+    <main
+      className="relative min-h-screen w-screen flex flex-col"
+      ref={containerRef}
+    >
       <div className="fixed bottom-0 left-0 w-full">
         <Navbar />
       </div>
 
-      <DragFolder id="Personal" ref={targerRef} img={folderLogo} placeholder="Personal" />
+      <DragFolder
+        id="Personal"
+        ref={targerRef}
+        img={folderLogo}
+        placeholder="Personal"
+      />
     </main>
   );
 }
